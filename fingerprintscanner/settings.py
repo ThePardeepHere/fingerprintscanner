9a829ede92d4742e587aaa3cfc35ce3b40937a81
Pardeep <thepardeephere@gmail.com>
2022-03-21 21:49:24 +0530

    added fike

--- fingerprintscanner/settings.py
+++ fingerprintscanner/settings.py
@@ -121,6 +121,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
+MAX_UPLOAD_SIZE = 104857600
+FILE_UPLOAD_MAX_MEMORY_SIZE = 10485760
+DATA_UPLOAD_MAX_MEMORY_SIZE = 10485760
+
 
 STATIC_URL = '/static/'
 # manually added
